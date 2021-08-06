c78bd557ce448f77fcc42f88742171cfa1dbceda
Mordecai Velasco <59404502+mrdcvlsc@users.noreply.github.com>
2021-08-06 09:29:35 +0800

    Add files via upload

---
+++ cyfre.hpp
@@ -0,0 +1,25 @@
+#ifndef CYFRE_HPP
+#define CYFRE_HPP
+
+#include "matrix_class.hpp"
+#include "matrix_operations.hpp"
+#include "matrix_transform.hpp"
+#include "matrix_stats.hpp"
+#include "scalar_operations.hpp"
+
+/*
+        INCLUDES ALL FILES IN THE CYFRE LIBRARY
+*/
+
+/* compiler -D flags
+
+CHECK_RANGE_DISABLE  -  Disable internal range checking, slightly speeds up the program (do not use in development)
+CHECK_SHAPE_DISABLE  -  Disable internal shape checking of matricies in matrix operations, slightly speeds up the program (do not use in development)
+OMPTHREAD            -  Enable OpenMP multi-threading / must include the openmp library -fopenmp
+
+DISPLAY_FUNC_CALLS   -  Show console logs of function calls with duration
+
+
+*/
+
+#endif
---
+++ scalar_operations.hpp
@@ -0,0 +1,65 @@
+#ifndef SCALAR_OPERATIONS_HPP
+#define SCALAR_OPERATIONS_HPP
+
+#include "matrix_class.hpp"
+
+namespace cyfre
+{
+    // /// @arg accepts a vector of iterators that
+    // /// @returns a copy of the dot product of two std::vector
+    // template<typename T>
+    // inline T dot(std::vector<T> tuple1, std::vector<T> tuple2)
+    // {
+    //     size_t length = tuple1.size();
+
+    //     #ifndef CHECK_SHAPE_DISABLE
+    //     if(length!=tuple2.size())
+    //     {
+    //         std::cerr<<"\n\nERROR : T dot(std::vector<typename std::vector<T>> tuple1, std::vector<typename std::vector<T>> tuple2)\n";
+    //         std::cerr<<"\tarray don't have the same lenght\n";
+    //         std::cout<<"\ttuple1 length : "<<tuple1.size()<<'\n';
+    //         std::cout<<"\ttuple2 length : "<<tuple2.size()<<'\n';
+    //         exit(1); 
+    //     }
+    //     #endif
+        
+    //     T summation = (T)0;
+
+    //     for(size_t i=0; i<length; ++i) summation += tuple1[i]*tuple2[i];
+
+    //     return summation;
+    // }
+
+    /// @arg accepts a cyfre::mat<T> objects
+    /// @returns T total, the total sum of all the elements of the matrix
+    template<typename T>
+    T total(const mat<T>& input)
+    {
+        T total = 0, zero = 0;
+        for(auto row: input.matrix) total+=std::accumulate(row.begin(),row.end(),zero);
+        return total;
+    }
+
+    /// @arg accepts a cyfre::mat<T> objects
+    /// @returns T trace_total, the total sum of the main diagonal
+    template<typename T>
+    T trace(const mat<T>& input)
+    {
+        #ifndef CHECK_SHAPE_DISABLE
+        if(input.width!=input.height)
+        {
+            std::cerr<<"\n\nERROR : mat.trace()\n";
+            std::cerr<<"\tthe matrix is not a square matrix, cannot get trace\n";
+            exit(1);
+        }
+        #endif
+
+        T trace_total = 0;
+        for(size_t i=0; i<input.height; ++i) trace_total+=input.matrix[i*input.width+i];
+        return trace_total;
+    }
+
+    // determinant
+}
+
+#endif
